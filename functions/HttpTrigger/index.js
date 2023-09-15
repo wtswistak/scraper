6d4fe9e2efea146db2c81eb0725adac9e2730fac
wojtek <wswistak@student.agh.edu.pl>
2023-09-15 15:06:20 +0200

    add new site to scrapping, fix polish chars

--- functions/HttpTrigger/index.js
+++ functions/HttpTrigger/index.js
@@ -1,40 +1,28 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
 
 module.exports = async function (context, req) {
   const searchQuery = req.query.query || (req.body && req.body.query);
 
-  // if (!searchQuery) {
-  //   context.res = {
-  //     status: 400,
-  //     body: "Brak wyszukiwania",
-  //   };
-  //   return;
-  // }
-
   try {
-    // const url = `https://www.wirtualnemedia.pl/wyniki/query:${encodeURIComponent(
-    //   searchQuery
-    // )}`;
-    const url = `https://www.national-geographic.pl/search/articles?q=${encodeURIComponent(
+    const urlFirst = `https://www.national-geographic.pl/search/articles?q=${encodeURIComponent(
       searchQuery
     )}&tab=articles`;
+    const urlSecond = `https://wyborcza.pl/0,91794.html?offset=0&searchType=ARTICLE&query=${encodeURIComponent(
+      searchQuery
+    )}&orderBy=accuracy`;
 
-    const response = await axios.get(url);
-    const html = response.data;
-    const $ = cheerio.load(html);
     const articles = [];
+    const responseFirst = await axios.get(urlFirst);
+    const htmlFirst = responseFirst.data;
+    const $first = cheerio.load(htmlFirst);
 
-    $(".col-md-6 ").each((index, element) => {
-      // const articleTitle = $(element).find(".news-title").text().trim();
-      // const articleLink = $(element).find(".subsite-box").attr("href");
-      // const articleDate = $(element).find(".news-date").text();
-      // const articleImg = $(element).find("img").attr("src");
-
-      const articleTitle = $(element).find("h2").text().trim();
-      const articleLink = $(element).find(".type-content-").attr("href");
-      const articleDate = $(element).find(".item-date").text();
-      const articleImg = $(element).find("img").attr("src");
+    $first(".col-md-6 ").each((index, element) => {
+      const articleTitle = $first(element).find("h2").text().trim();
+      const articleLink = $first(element).find(".type-content-").attr("href");
+      const articleDate = $first(element).find(".item-date").text();
+      const articleImg = $first(element).find("img").attr("src");
 
       articles.push({
         title: articleTitle,
@@ -43,6 +31,32 @@ module.exports = async function (context, req) {
         img: articleImg,
       });
     });
+
+    const responseSecond = await axios.get(urlSecond, {
+      responseType: "arraybuffer",
+    });
+    const htmlSecond = iconv.decode(responseSecond.data, "iso-8859-2");
+    const $Second = cheerio.load(htmlSecond);
+
+    $Second(".results__result").each((index, element) => {
+      const articleTitle = $Second(element)
+        .find(".result__header-ref")
+        .attr("title");
+      const articleLink = $Second(element)
+        .find(".result__header-ref")
+        .attr("href");
+      const articleDate = $Second(element).find(".result__date").text();
+      const articleImg = $Second(element).find("img").attr("src");
+
+      if (!articleTitle) return;
+      articles.push({
+        title: articleTitle,
+        link: articleLink,
+        date: articleDate,
+        img: articleImg,
+      });
+    });
+
     const data = {
       searchQuery: searchQuery,
       articles: articles,
@@ -50,7 +64,10 @@ module.exports = async function (context, req) {
 
     context.res = {
       status: 200,
-      body: data,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data),
     };
   } catch (error) {
     context.res = {
