ea2849bc4adb8f0a8301348da89a5c81f77ad87e
wojtek <wswistak@student.agh.edu.pl>
2023-09-29 10:47:31 +0200

    style

--- src/components/results/LoadMoreBtn.jsx
+++ src/components/results/LoadMoreBtn.jsx
@@ -1,7 +1,7 @@
 function LoadMoreBtn({ handleLoadMore }) {
   return (
     <button
-      className="font-medium text-sm bg-[#124736] py-3 px-6 rounded-xl text-[#ececec] mt-4"
+      className="font-medium text-sm bg-[#18694d] py-3 px-6 rounded-3xl duration-200 text-[#ececec] mt-4 hover:bg-[#185341]"
       onClick={handleLoadMore}
     >
       Załaduj więcej
